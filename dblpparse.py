32a505054f62ddd0ae5bc764cc62715b6872190b
Robert Escriva <me@robescriva.com>
2012-06-05 21:40:34 -0400

    Import hte IEEE Computer Journal

--- dblpparse.py
+++ dblpparse.py
@@ -605,7 +605,7 @@ d.add_journal('queue',          'ACM Queue')
 d.add_journal('ccr',            'CCR', 'SIGCOMM Computer Communications Review', dblpname='Computer Communication Review')
 d.add_journal('osr',            'OSR', 'SIGOPS Operating Systems Review', dblpname='Operating Systems Review', dblpslug='sigops')
 # IEEE publications
-d.add_journal('ieeecomputer',   'IEEE Computer')
+d.add_journal('ieeecomputer',   'IEEE Computer', dblpslug='computer')
 d.add_journal('ieeeconcurrency', 'IEEE Concurrency')
 d.add_journal('ieeeis_selfstar', 'IEEE IS', 'IEEE Intelligent Systems, Special Issue on Self-Management through Self-Organization in Information Systems')
 d.add_journal('ieeeis',         'IEEE IS', 'IEEE Intelligent Systems')
