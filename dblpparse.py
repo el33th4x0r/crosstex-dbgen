6d92564d007d1fe2b6bceb624d752133ff0e8def
Robert Escriva <me@robescriva.com>
2012-06-05 21:56:47 -0400

    Import the IEEE Intelligent Systems Journal

--- dblpparse.py
+++ dblpparse.py
@@ -607,7 +607,7 @@ d.add_journal('osr',            'OSR', 'SIGOPS Operating Systems Review', dblpna
 # IEEE publications
 d.add_journal('ieeecomputer',   'IEEE Computer', dblpslug='computer')
 d.add_journal('ieeeconcurrency', 'IEEE Concurrency', dblpslug='ieeecc')
-d.add_journal('ieeeis',         'IEEE IS', 'IEEE Intelligent Systems')
+d.add_journal('ieeeis',         'IEEE IS', 'IEEE Intelligent Systems', dblpname='IEEE Intelligent Systems', dblpslug='expert')
 d.add_journal('ieeenetwork',    'IEEE Network Magazine')
 d.add_journal('ieeesac',        'IEEE Journal on Selected Areas in Communications')
 d.add_journal('ieeesecpriv',    'IEEE Security and Privacy')
